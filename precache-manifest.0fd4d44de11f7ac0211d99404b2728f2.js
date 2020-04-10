44d5ca8f0b7fed9b1f75c2e6aaebc8fe28f24ebe
Francisco Raigoza <raigoza.frankie@gmail.com>
2020-04-09 18:58:58 -0700

    Updates

---
+++ precache-manifest.0fd4d44de11f7ac0211d99404b2728f2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0abf246cd13f8aed62a5569f142ed532",
+    "url": "/coronavirus-tracker/index.html"
+  },
+  {
+    "revision": "c7267f8774ba44aa0374",
+    "url": "/coronavirus-tracker/static/css/main.36125af6.chunk.css"
+  },
+  {
+    "revision": "119761eb6f0ce1005b8e",
+    "url": "/coronavirus-tracker/static/js/2.53d51e67.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/coronavirus-tracker/static/js/2.53d51e67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c7267f8774ba44aa0374",
+    "url": "/coronavirus-tracker/static/js/main.0f74d678.chunk.js"
+  },
+  {
+    "revision": "35c858d73af1965f2e45",
+    "url": "/coronavirus-tracker/static/js/runtime-main.39f88733.js"
+  },
+  {
+    "revision": "b91d250aa567c8612e9cfb3f9533fbb1",
+    "url": "/coronavirus-tracker/static/media/COVID_Banner2_small.b91d250a.png"
+  }
+]);
